190e0ff17d9c33382133de265ad0b60e519176e3
JC Lucas <jclucasasp@gmail.com>
2021-06-16 19:21:26 +0200

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipiesListComponent } from './recipes/recipies-list/recipies-list.component';
+import { RecipiesDetailComponent } from './recipes/recipies-detail/recipies-detail.component';
+import { RecipiesItemComponent } from './recipes/recipies-list/recipies-item/recipies-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipiesListComponent,
+    RecipiesDetailComponent,
+    RecipiesItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
